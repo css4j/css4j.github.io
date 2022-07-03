3cf38a5f6233dd10c7cf8d6bb81e3736068c5f03
Carlos Amengual <carlosame644+github@gmail.com>
2022-07-03 14:39:37 +0200

    Add modular javadocs

---
+++ api/latest/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"io.sf.carte.css4j"},{"l":"io.sf.carte.css4j.agent.net"},{"l":"io.sf.carte.css4j.awt"},{"l":"io.sf.carte.css4j.dom4j"},{"l":"io.sf.carte.tokenproducer"},{"l":"io.sf.carte.util"},{"l":"io.sf.carte.xml.dtd"}];updateSearchResults();
